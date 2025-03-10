c4f0919ca3cfb6a9718fc8c1fa561c4625f53dd8
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 22:14:04 +0000

    Add AdminPanel.jsx component

---
+++ AdminPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminPanel() {
+    return (
+        <div className="admin-panel">
+            <h2>Admin Management</h2>
+            {/* Admin functionalities will be rendered here */}
+        </div>
+    );
+}
+
+export default AdminPanel;
