08555431799184019fa2853351698a0da0c1a8a2
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 22:14:02 +0000

    Add ShoppingCart.jsx component

---
+++ ShoppingCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ShoppingCart() {
+    return (
+        <div className="shopping-cart">
+            <h2>Your Shopping Cart</h2>
+            {/* Cart items will be rendered here */}
+        </div>
+    );
+}
+
+export default ShoppingCart;
