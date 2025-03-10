d48fb9164fcea8fd6473cb7bbf72c3a39b5e15b2
yrks12 <128397979+yrks12@users.noreply.github.com>
2025-03-10 22:14:00 +0000

    Add ProductList.jsx component

---
+++ ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductList() {
+    return (
+        <div className="product-list">
+            <h2>Featured Products</h2>
+            {/* Product items will be rendered here */}
+        </div>
+    );
+}
+
+export default ProductList;
